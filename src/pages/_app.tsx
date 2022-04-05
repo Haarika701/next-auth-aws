a95029ea417170a2ca1868c02c6903aa2dc91c3f
HaarikaR <hramadugu@gmail.com>
2022-04-05 12:18:53 -0700

    chore:project setup completed

---
+++ src/pages/_app.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import type { AppProps } from "next/app";
+import theme from "../theme";
+import { useEffect } from "react";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  });
+  return (
+    <>
+      <Head>
+        <title>Reddit Clone</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
